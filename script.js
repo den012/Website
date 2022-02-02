e9679bb69288c51cea87c52509411fd86d2acf97
Denis <69869262+den012@users.noreply.github.com>
2022-02-02 22:49:22 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function openPage(){
     window.location="index2.html"
 }
+
+const currentPage = location.href
+const navItems = document.querySelectorAll('a')
+for(let i = 0; i < navItems.length; i++){
+    if(navItems[i].href == currentPage){
+        navItems[i].className = "is-active"
+    }
+}
